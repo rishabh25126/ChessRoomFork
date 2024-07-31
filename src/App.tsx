186ae9d88c8b4ec207a142e7af080b8be62001c3
Bhaskar Das <bhaskar.das9476@gmail.com>
2024-07-31 12:34:52 +0530

    Added Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <p>
+        <p className="text-3xl font-bold underline">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <Quotes />
